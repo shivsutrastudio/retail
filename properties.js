e2e88088e973d66ae0a717560e634d6b2ba1e0d4
shivsutrastudio <shivsutrastudio@gmail.com>
2026-03-10 17:48:12 +0530

    Add files via upload

--- properties.js
+++ properties.js
@@ -23,13 +23,13 @@ price:5000000+(i*250000),
 
 images:[
 
-"https://images.unsplash.com/photo-1505691938895-1758d7feb511",
-"https://images.unsplash.com/photo-1556911220-e15b29be8c8f",
-"https://images.unsplash.com/photo-1505693416388-ac5ce068fe85",
-"https://images.unsplash.com/photo-1584622781564-1d987f7333c1",
-"https://images.unsplash.com/photo-1502005097973-6a7082348e28",
-"https://images.unsplash.com/photo-1592595896616-c37162298647",
-"https://images.unsplash.com/photo-1503376780353-7e6692767b70"
+`https://source.unsplash.com/800x600/?modern-house,${i}`,
+`https://source.unsplash.com/800x600/?living-room,${i}`,
+`https://source.unsplash.com/800x600/?kitchen,${i}`,
+`https://source.unsplash.com/800x600/?bedroom,${i}`,
+`https://source.unsplash.com/800x600/?bathroom,${i}`,
+`https://source.unsplash.com/800x600/?balcony,${i}`,
+`https://source.unsplash.com/800x600/?parking,${i}`
 
 ]
 
